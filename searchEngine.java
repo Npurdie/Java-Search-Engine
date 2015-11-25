359bb60914e56bea2a07ded39ff001e5c933a276
Nick Purdie <nick.purdie@mail.mcgill.ca>
2015-11-25 15:05:08 -0500

    update "

--- searchEngine.java
+++ searchEngine.java
@@ -7,8 +7,6 @@ public class searchEngine {
     public HashMap<String,LinkedList<String>> wordIndex;		
     public directedGraph internet;             // this is our internet graph
     
-    
-    
     // Constructor initializes everything to empty data structures
     // It also sets the location of the internet files
     searchEngine() {
@@ -16,154 +14,113 @@ public class searchEngine {
 	htmlParsing.internetFilesLocation = "internetFiles";
 	wordIndex = new HashMap<String, LinkedList<String> > ();		
 	internet = new directedGraph();				
-    } // end of constructor2015
-    
+    }
     
     // Returns a String description of a searchEngine
     public String toString () {
 	return "wordIndex:\n" + wordIndex + "\ninternet:\n" + internet;
     }
     
-    
     // This does a graph traversal of the internet, starting at the given url.
     // For each new vertex seen, it updates the wordIndex, the internet graph,
     // and the set of visited vertices.
-    
     void traverseInternet(String url) throws Exception {
-    	internet.addVertex(url);
-    	internet.setVisited(url,true);
-    		//parse the current url and put it in the word index hashmap
-    	LinkedList<String> content = htmlParsing.getContent(url);
+    	internet.addVertex(url);		//add vertex to map
+    	internet.setVisited(url,true);	//avoids DDOS by only visiting a webpage once
+    		
+    	LinkedList<String> content = htmlParsing.getContent(url);	//parse the current url and put it in the word index hashmap
     	Iterator<String> itr1 = content.iterator();
-    	while (itr1.hasNext())	{
+
+    	while (itr1.hasNext())	{	//for all words in webpage content
     		String s = itr1.next();
     		if (wordIndex.containsKey(s))	{
     			if (!wordIndex.get(s).contains(url))	{
-    				wordIndex.get(s).addLast(url);
+    				wordIndex.get(s).addLast(url);		//add word to wordIndex
     			}
     		}
-    		else	{
+    		else	{	//if website isnt part of wordlist yet, add it
     			LinkedList<String> newList = new LinkedList<String>();
     			newList.addLast(url);
     			wordIndex.put(s,newList);
     		}
     	}
 
-    	LinkedList<String> links = htmlParsing.getLinks(url);
-
+    	LinkedList<String> links = htmlParsing.getLinks(url);	//parse current url for links, into a linkedList
     	Iterator<String> itr = links.iterator();
-    	while (itr.hasNext()) {
+
+    	while (itr.hasNext()) {		//for every link found on webpage
 			String s = itr.next();
 			internet.addEdge(url, s);
 			if (!(internet.getVisited(s)))	{
-				traverseInternet(s);
+				traverseInternet(s);		//recursive call of traverseInternet
 			}
 		}
-
-	/* WRITE SOME CODE HERE */
-	
-	/* Hints
-	   0) This should take about 50-70 lines of code (or less)
-	   1) To parse the content of the url, call
-	   htmlParsing.getContent(url), which returns a LinkedList of Strings 
-	   containing all the words at the given url. Also call htmlParsing.getLinks(url).
-	   and assign their results to a LinkedList of Strings.
-	   2) To iterate over all elements of a LinkedList, use an Iterator,
-	   as described in the text of the assignment
-	   3) Refer to the description of the LinkedList methods at
-	   http://docs.oracle.com/javase/6/docs/api/ .
-	   You will most likely need to use the methods contains(String s), 
-	   addLast(String s), iterator()
-	   4) Refer to the description of the HashMap methods at
-	   http://docs.oracle.com/javase/6/docs/api/ .
-	   You will most likely need to use the methods containsKey(String s), 
-	   get(String s), put(String s, LinkedList l).  
-	*/
-	
-    } // end of traverseInternet
+    }
     
-    
-    /* This computes the pageRanks for every vertex in the internet graph.
-       It will only be called after the internet graph has been constructed using 
-       traverseInternet.
-       Use the iterative procedure described in the text of the assignment to
-       compute the pageRanks for every vertices in the graph. 
-       
-       This method will probably fit in about 30 lines.
-    */
     void computePageRanks() {
+    	/*
+		* Page Ranks are calculated from this equation : P(v) = 0.5+ 0.5*(P(v1)/O(v1) + P(v2)/O(v2) + ... + P(vn)/O(vn))
+		* Where P(v) is the pageRank of vertext v and v1, v2, ... vn is every vertex that points to v
+		*/
+
     	//initialize all pageranks to 1
     	Iterator<String> itr1 = internet.getVertices().iterator() ;
     	while (itr1.hasNext()) {
 			internet.setPageRank(itr1.next(), 1);
 		}
 
-    	for (int i=0; i<100; i++) {	//repeat 100 times for convergence
+    	for (int i=0; i<100; i++)	{	//this is not exact but repeat 100 times for ~ convergence
     		Iterator<String> itr = internet.getVertices().iterator() ;
-	    	while (itr.hasNext()) {
+
+	    	while (itr.hasNext()) {		//repeat for every url visited
 	    		String vertex = itr.next();
 	    		double temp = 0.0;
 	    		Iterator<String> itr2 = internet.getEdgesInto(vertex).iterator();
-				while (itr2.hasNext()) {
-					String s = itr2.next();
-					temp = temp + internet.getPageRank(s)/internet.getOutDegree(s);
-				}
-				internet.setPageRank(vertex, 0.5+0.5*temp);
-			}
-		}
 
-	
-    } // end of computePageRanks
+				while (itr2.hasNext()) {	//repeat for every website that points to this url
+					String s = itr2.next();
+					temp = temp + internet.getPageRank(s)/internet.getOutDegree(s);		//temp is the sum of the pageRanks/outDegree of every site that points to the url
+				}
+				internet.setPageRank(vertex, 0.5+0.5*temp);  // calculate pageRank
+			}
+		}	
+    }
     
-	
-    /* Returns the URL of the page with the high page-rank containing the query word
-       Returns the String "" if no web site contains the query.
-       This method can only be called after the computePageRanks method has been executed.
-       Start by obtaining the list of URLs containing the query word. Then return the URL 
-       with the highest pageRank.
-       This method should take about 25 lines of code.
-    */
-    String getBestURL(String query) {
+    String getBestURL(String query) {	//based on a querry, return the most relevent url based on it's score calculated in computePageRank
     	LinkedList<String> queryMatch = new LinkedList<String>();
-    	if (wordIndex.containsKey(query)) {
+
+    	if (wordIndex.containsKey(query)) {		//check for sites containing the query
     		queryMatch = wordIndex.get(query);
     	}
-    	else return "";
+    	else return "";	// return an empty string if not websites contain the query
     	
     	String highestRank = "";
+		Iterator<String> itr = queryMatch.iterator() ;
 
-    	Iterator<String> itr = queryMatch.iterator() ;
-    	while (itr.hasNext()) {
+    	while (itr.hasNext()) {		//for all urls that contain the query
     		String temp = itr.next();
-			if(internet.getPageRank(temp) > internet.getPageRank(highestRank))	{
+			if(internet.getPageRank(temp) > internet.getPageRank(highestRank))	{	// parse the list continuously adding a higher ranked url if it exists
 				highestRank = temp;
 			}
 		}
-	return highestRank;
-    } // end of getBestURL
+		return highestRank;
+    }
     
     
 	
     public static void main(String args[]) throws Exception{		
-	searchEngine mySearchEngine = new searchEngine();
-	// to debug your program, start with.
-		//mySearchEngine.traverseInternet("http://www.cs.mcgill.ca/~blanchem/250/a.html");
-	
-	// When your program is working on the small example, move on to
-	 mySearchEngine.traverseInternet("http://www.cs.mcgill.ca");
+		searchEngine mySearchEngine = new searchEngine();
+		mySearchEngine.traverseInternet("http://www.cs.mcgill.ca");
+		mySearchEngine.computePageRanks();
 
-	mySearchEngine.computePageRanks();
-
-	System.out.println(mySearchEngine);
-
-	BufferedReader stndin = new BufferedReader(new InputStreamReader(System.in));
-	String query;
-	do {
-	    System.out.print("Enter query: ");
-	    query = stndin.readLine();
-	    if ( query != null && query.length() > 0 ) {
-		System.out.println("Best site = " + mySearchEngine.getBestURL(query));
-	    }
-	} while (query!=null && query.length()>0);				
+		BufferedReader stndin = new BufferedReader(new InputStreamReader(System.in));
+		String query;
+		do {
+		    System.out.print("Enter query: ");
+		    query = stndin.readLine();
+		    if ( query != null && query.length() > 0 ) {
+			System.out.println("Best site = " + mySearchEngine.getBestURL(query));
+		    }
+		} while (query!=null && query.length()>0);				
     } // end of main
 }
